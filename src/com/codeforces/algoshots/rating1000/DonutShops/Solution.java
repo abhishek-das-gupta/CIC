9ff6725122e7d9bff8629c740bdb47de99b8d7b9
onTheQT <abhishekdasgupta005@gmail.com>
2021-02-28 19:34:14 +0530

    AC solution for 339B. (Modulo arithmetic)

--- src/com/codeforces/algoshots/rating1000/DonutShops/Solution.java
+++ src/com/codeforces/algoshots/rating1000/DonutShops/Solution.java
@@ -3,27 +3,14 @@ package com.codeforces.algoshots.rating1000.DonutShops;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Solution {
     public static void main(String[] args) {
         FastScanner fs = new FastScanner();
-        int n = fs.nextInt(), m = fs.nextInt(), t = 0;
-        int[] a = fs.readArray(m);
-        int addCycle = 0;
-        int[] cycleAddedHouses = new int[m];
-        for (int i = 0; i < a.length; ++i) {
-            if (i > 0 && a[i - 1] > a[i]) {
-                addCycle += n;
-            }
-            cycleAddedHouses[i] += a[i] + addCycle;
-        }
-        //System.out.println(Arrays.toString(a));
-        for (int i = 0; i < cycleAddedHouses.length; ++i) {
-            t += i == 0 ? cycleAddedHouses[i] - 1 : cycleAddedHouses[i] - cycleAddedHouses[i - 1];
-        }
-        System.out.println(t);
+        // x*a < (c / b)
+        // (a + )
+
     }
 
     static class FastScanner {
